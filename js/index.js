2cee29822ae9b881edb5bc1eb0eef03b22fd55b9
Vinh <nguyenthevinh1997@gmail.com>
2019-08-01 11:11:54 +0700

    Finish service section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-    
-    $('#p1-bannerinfo').delay = 1.2s;
-    $('#p1-bannerinfo').opacity = 0;
-    
+  $("#p1-bannerinfo").delay = 1.2;
+  $("#p1-bannerinfo").opacity = 0;
+});
+$(document).ready(function() {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function() {
+    /* Check the location of each desired element */
+    $(".main-section").each(function(i) {
+      var bottom_of_object = $(".fadeServices").offset().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      console.log(bottom_of_object + "SS" + bottom_of_window);
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window >= bottom_of_object) {
+        $(".main-section").animate({'opacity':'1'});
+        $(".fadeServices").addClass("fadeInLeft");
+        $(".fadeInUpImg").addClass("fadeInUp");
+      }
+    });
+  });
 });
